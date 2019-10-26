51df2d7f7cfa5d76cdf112c55dcac6b3fb51796f
Tailbeast <55963884+taildeast@users.noreply.github.com>
2019-10-26 22:34:00 +0900

    Add files via upload
    테스트용(팀장)

---
+++ ProjectLV.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int Lv = 0;
+int Exp = 0;
+
+void LV(int ex)
+{
+	Exp += ex;
+	while (Exp >= 100)
+	{
+		Exp -= 100;
+		Lv++;
+	}
+	cout << "LV : " << Lv << " exp : " << Exp << endl;
+}
+
+void main() {
+	LV(5);
+	LV(95);
+	LV(2951);
+}
